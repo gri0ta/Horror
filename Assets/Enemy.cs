388b9135d4a713f50a443381e5741ce5b8730f32
vik <39262485+ViktorKornilov@users.noreply.github.com>
2023-12-21 17:01:33 +0200

    AI

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float viewDistance = 20;
+    public float wanderDistance = 10;
+    public float speed;
+    public Transform target;
+
+    Rigidbody rb;
+    NavMeshAgent agent;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        var distance = Vector3.Distance(transform.position, target.position);
+
+        if (distance < viewDistance)
+        {
+            // CHASE
+            agent.destination = target.position;
+        }
+        else
+        {
+            // SEEK
+            // Random.Range(0,120) == 1
+            if (agent.velocity == Vector3.zero)
+            {
+                agent.destination = Random.insideUnitSphere * wanderDistance;
+            }
+        }
     }
 }
